efb8fc2ea2df0264f2e2dcf794fd2b373faa79cc
victor-430 <oluwatobivictor430@gmail.com>
2025-05-31 12:45:53 +0100

    added root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata: Metadata = {
+  title: "Hospital Management System",
+  description: "Making life easier and better",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
