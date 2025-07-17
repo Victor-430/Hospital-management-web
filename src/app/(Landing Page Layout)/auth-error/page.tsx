f863b48e54764f836bc1adf4249b219c33615c23
victor-430 <oluwatobivictor430@gmail.com>
2025-07-17 13:33:33 +0100

    fixed useSearchParam error - wrapped autherrorpage with suspense

--- src/app/(Landing Page Layout)/auth-error/page.tsx
+++ src/app/(Landing Page Layout)/auth-error/page.tsx
@@ -1,7 +1,13 @@
+import Loading from "@/app/(Dashboard Layout)/loading";
 import { AuthError } from "@/components/auth/AuthError";
+import { Suspense } from "react";
 
 const AuthErrorpage = () => {
-  return <AuthError />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <AuthError />;
+    </Suspense>
+  );
 };
 
 export default AuthErrorpage;
